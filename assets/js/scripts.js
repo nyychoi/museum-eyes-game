db3bc53d66cced2db603a6138a04c2eb37e1f9b5
Colin Hart <colin.hart@simplybusiness.com>
2020-10-25 14:38:41 -0400

    still bad

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,10 +2,15 @@ $(document).ready(function(){
   const cardsArray = [
     'card_1.png', 'card_2.png', 'card_3.png', 'card_4.png', 'card_5.png', 'card_6.png', 'card_7.png', 'card_8.png', 'card_9.png',
   ]
-  let cardCache = 0;
-
+  let cardCache = 1;
   $('.button').click(function() {
     cardCache = (cardCache + 1) % cardsArray.length;
-    $('img').attr('src', `assets/images/card_${cardCache}.png`)
+    $('.under').attr('src', `assets/images/card_${cardCache}.png`)
+    $(".over").before($(".under"));
+    $('#card-a').toggleClass('over under')
+    $('#card-b').toggleClass('under over')
+
   });
+
+  
 })
