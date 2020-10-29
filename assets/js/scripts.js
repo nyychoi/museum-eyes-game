fa3b5735eca19867f2fe3715ba553dc35755567a
Colin Hart <colin.hart@simplybusiness.com>
2020-10-28 22:16:49 -0400

    Clean up

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,44 +1,56 @@
-$(document).ready(function(){
-  const cardNumber = 10;
-  // [
-  //   'card_1.png', 'card_2.png', 'card_3.png', 'card_4.png', 'card_5.png', 'card_6.png', 'card_7.png', 'card_8.png', 'card_9.png',
-  // ]
+$(document).ready(() =>{
+  let cardNumber = 10;
+  let $button = $(`.button`);
+  let $toggle = $(`.toggle`);
+  let $img = $(`img`)
   let cardCache = 0;
   let firstClick = true;
-  let $img;
+  let transitionSpeed = 500;
+  let updateCardCache;
+  let transitionBetweenCards;
+  let toggleButtonText;
+  let displayFirstCard;
 
-
-  $('.button').click(function() {
+  $button.click(() => {
     if (firstClick === true ) {
-      $('img').toggle(1000); firstClick = false; 
-      cardCache = (cardCache + 1) % cardNumber;
+      $(`img`).toggle(transitionSpeed); firstClick = false;
+      updateCardCache()
       return
     }
 
-    $(this).text(function(i, text) {
-
-      console.log(cardCache, cardNumber)
-        if (cardCache === cardNumber - 1) {
-          return 'start over';
-        } else {
-          return 'pick a card';
-        }
-    });
-
-    $img = $('img').detach()
-    $img.attr('src', `assets/images/card_${cardCache}.png`)
-        .css('display','none')
-        .appendTo('.card')
-        .show(1500)
-
-    cardCache = (cardCache + 1) % cardNumber;
+    toggleButtonText()
+    transitionBetweenCards();
+    updateCardCache()
   });
-  
 
-  $(".toggle").click(function(){
-    $(".instructions").toggle(1000);
-    $(this).text(function(i, text) {
-      return text === 'hide instructions' ? 'show instructions' : 'hide instructions';
+  $toggle.click(() => {
+    $(`.instructions`).toggle(transitionSpeed);
+    $(this).text((i, text) => {
+      return text === `hide instructions` ? `show instructions` : `hide instructions`;
     });
   })
+
+  updateCardCache = function() {
+    cardCache = (cardCache + 1) % cardNumber;
+  };
+
+  transitionBetweenCards = function() {
+    $img.detach()
+      .attr(`src`, `assets/images/card_${cardCache}.png`)
+      .css(`display`,`none`)
+      .appendTo(`.card`)
+      .show(transitionSpeed)
+  }
+
+    toggleButtonText = function() {
+      $button.text((text) => {
+          if (cardCache === cardNumber - 1) {
+            return 'start over';
+          } else {
+            return 'pick a card';
+          }
+      });
+    }
+
+
 })
